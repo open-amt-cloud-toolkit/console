5a2a96c8cde464c15126847c3e1fb274d6176c18
Mike Johanson <michael.johanson@intel.com>
2024-05-01 15:47:36 -0700

    feat: enable browser launch

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/open-amt-cloud-toolkit/console/config"
 	"github.com/open-amt-cloud-toolkit/console/internal/app"
@@ -14,6 +17,35 @@ func main() {
 		log.Fatalf("Config error: %s", err)
 	}
 
+	if os.Getenv("GIN_MODE") != "debug" {
+		go func() {
+			browserError := openBrowser("http://localhost:" + cfg.HTTP.Port)
+
+			if browserError != nil {
+				panic(browserError)
+			}
+		}()
+	}
 	// Run
 	app.Run(cfg)
+}
+
+func openBrowser(url string) error {
+	var cmd string
+
+	var args []string
+
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = "open"
+		args = []string{url}
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start", url}
+	default:
+		cmd = "xdg-open"
+		args = []string{url}
+	}
+
+	return exec.Command(cmd, args...).Start()
 }
