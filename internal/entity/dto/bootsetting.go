8138092fe1849d4a61b4f59a417f25b56fef7c52
Mike <mjohanson@gesdate.com>
2024-05-08 15:13:01 -0700

    fix: set boot config role

--- internal/entity/dto/bootsetting.go
+++ internal/entity/dto/bootsetting.go
@@ -2,5 +2,5 @@ package dto
 
 type BootSetting struct {
 	Action int  `json:"action" binding:"required" example:"8"`
-	UseSOL bool `json:"useSOL" binding:"required" example:"true"`
+	UseSOL bool `json:"useSOL" binding:"omitempty,required" example:"true"`
 }
