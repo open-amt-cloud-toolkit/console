2d3bc4f642df141b649e97b2204fc8dd4318f1d2
Mike Johanson <michael.johanson@intel.com>
2024-07-09 14:04:59 -0700

    feat: use dto in getfeatures

--- internal/controller/http/v2/devicemanagement_test.go
+++ internal/controller/http/v2/devicemanagement_test.go
@@ -8,10 +8,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	power "github.com/open-amt-cloud-toolkit/go-wsman-messages/v2/pkg/wsman/cim/power"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 
@@ -38,9 +36,7 @@ func deviceManagementTest(t *testing.T) (*MockDeviceManagementFeature, *gin.Engi
 	return deviceManagement, engine
 }
 
-var aGoodTime = time.Unix(int64(1073007983), 0)
-
-func TestGetNetworkSettings(t *testing.T) {
+func TestGetFeatures(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
@@ -53,170 +49,15 @@ func TestGetNetworkSettings(t *testing.T) {
 		response     interface{}
 	}{
 		{
-			name:   "getVersion - successful retrieval",
-			url:    "/api/v2/amt/version/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetVersion(context.Background(), "valid-guid").
-					Return(map[string]interface{}{"version": "1.0.0"}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     map[string]interface{}{"version": "1.0.0"},
-		},
-		{
 			name:   "getFeatures - successful retrieval",
 			url:    "/api/v2/amt/features/valid-guid",
 			method: http.MethodGet,
 			mock: func(m *MockDeviceManagementFeature) {
 				m.EXPECT().GetFeatures(context.Background(), "valid-guid").
-					Return(map[string]interface{}{"features": []string{"feature1", "feature2"}}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     map[string]interface{}{"features": []string{"feature1", "feature2"}},
-		},
-		{
-			name:        "setFeatures - successful setting",
-			url:         "/api/v2/amt/features/valid-guid",
-			method:      http.MethodPost,
-			requestBody: dto.Features{},
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().SetFeatures(context.Background(), "valid-guid", dto.Features{}).Return(dto.Features{}, nil)
+					Return(dto.Features{}, nil)
 			},
 			expectedCode: http.StatusOK,
 			response:     dto.Features{},
-		},
-		{
-			name:   "getAlarmOccurrences - successful retrieval",
-			url:    "/api/v2/amt/alarmOccurrences/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetAlarmOccurrences(context.Background(), "valid-guid").
-					Return([]dto.AlarmClockOccurrence{}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     []dto.AlarmClockOccurrence{},
-		},
-		{
-			name:   "deleteAlarmOccurrences - successful deletion",
-			url:    "/api/v2/amt/alarmOccurrences/valid-guid",
-			method: http.MethodDelete,
-			requestBody: dto.AlarmClockOccurrence{
-				ElementName:        "elementName",
-				StartTime:          aGoodTime,
-				Interval:           1,
-				DeleteOnCompletion: true,
-				InstanceID:         "1",
-			},
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().DeleteAlarmOccurrences(context.Background(), "valid-guid", "1").Return(nil)
-			},
-			expectedCode: http.StatusNoContent,
-		},
-		{
-			name:   "getHardwareInfo - successful retrieval",
-			url:    "/api/v2/amt/hardwareInfo/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetHardwareInfo(context.Background(), "valid-guid").
-					Return(map[string]interface{}{"hardware": "info"}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     map[string]interface{}{"hardware": "info"},
-		},
-		{
-			name:   "getPowerState - successful retrieval",
-			url:    "/api/v2/amt/power/state/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetPowerState(context.Background(), "valid-guid").
-					Return(map[string]interface{}{"state": "on"}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     map[string]interface{}{"state": "on"},
-		},
-		{
-			name:   "powerAction - successful action",
-			url:    "/api/v2/amt/power/action/valid-guid",
-			method: http.MethodPost,
-			requestBody: dto.PowerAction{
-				Action: 4,
-			},
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().SendPowerAction(context.Background(), "valid-guid", 4).
-					Return(power.PowerActionResponse{ReturnValue: 0}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     power.PowerActionResponse{ReturnValue: 0},
-		},
-		{
-			name:   "getAuditLog - successful retrieval",
-			url:    "/api/v2/amt/log/audit/valid-guid?startIndex=0",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetAuditLog(context.Background(), 0, "valid-guid").
-					Return(dto.AuditLog{}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     dto.AuditLog{},
-		},
-		{
-			name:   "getEventLog - successful retrieval",
-			url:    "/api/v2/amt/log/event/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetEventLog(context.Background(), "valid-guid").
-					Return([]dto.EventLog{}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     []dto.EventLog{},
-		},
-		{
-			name:   "setBootOptions - successful setting",
-			url:    "/api/v2/amt/power/bootOptions/valid-guid",
-			method: http.MethodPost,
-			requestBody: dto.BootSetting{
-				Action: 109,
-			},
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().SetBootOptions(context.Background(), "valid-guid", dto.BootSetting{
-					Action: 109,
-				}).Return(power.PowerActionResponse{ReturnValue: 0}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     power.PowerActionResponse{ReturnValue: 0},
-		},
-		{
-			name:   "successful retrieval",
-			url:    "/api/v2/amt/networkSettings/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetNetworkSettings(context.Background(), "valid-guid").
-					Return(map[string]interface{}{"": ""}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     map[string]interface{}{"": ""},
-		},
-		{
-			name:   "getCertificates - successful retrieval",
-			url:    "/api/v2/amt/certificates/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetCertificates(context.Background(), "valid-guid").
-					Return(dto.SecuritySettings{}, nil)
-			},
-			expectedCode: http.StatusOK,
-			response:     dto.SecuritySettings{},
-		},
-		{
-			name:   "getCertificates - failed retrieval",
-			url:    "/api/v2/amt/certificates/valid-guid",
-			method: http.MethodGet,
-			mock: func(m *MockDeviceManagementFeature) {
-				m.EXPECT().GetCertificates(context.Background(), "valid-guid").
-					Return(dto.SecuritySettings{}, ErrGeneral)
-			},
-			expectedCode: http.StatusInternalServerError,
-			response:     dto.SecuritySettings{},
 		},
 	}
 
