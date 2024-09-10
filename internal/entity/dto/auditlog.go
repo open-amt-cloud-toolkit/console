d6142257ada336a223d45d02179845cafb8a3e5e
Mike Johanson <michael.johanson@intel.com>
2024-09-10 14:49:59 -0700

    feat: restrict username length

--- internal/entity/dto/auditlog.go
+++ internal/entity/dto/auditlog.go
@@ -6,17 +6,3 @@ type AuditLog struct {
 	TotalCount int                       `json:"totalCnt" binding:"required" example:"0"`
 	Records    []auditlog.AuditLogRecord `json:"records" binding:"required"`
 }
-
-// type AuditLogRecord struct {
-// 	AuditAppID     int       `json:"AuditAppId" binding:"required" example:"0"`
-// 	EventID        int       `json:"EventId" binding:"required" example:"0"`
-// 	InitiatorType  int       `json:"InitiatorType" binding:"required" example:"0"`
-// 	AuditApp       string    `json:"AuditApp" binding:"required" example:"MikeIsAmazing"`
-// 	Event          string    `json:"Event" binding:"required" example:"MikeIsAmazing"`
-// 	Initiator      string    `json:"Initiator" binding:"required" example:"MikeIsAmazing"`
-// 	Time           time.Time `json:"Time" binding:"required" example:"MikeIsAmazing"`
-// 	MCLocationType int       `json:"MCLocationType" binding:"required" example:"0"`
-// 	NetAddress     string    `json:"NetAddress" binding:"required" example:"MikeIsAmazing"`
-// 	Ex             string    `json:"Ex" binding:"required" example:"MikeIsAmazing"`
-// 	ExStr          string    `json:"ExStr" binding:"required" example:"MikeIsAmazing"`
-// }
