9bcf0244d4bd44068df413e00f6cd9e85ad8cf24
madhavilosetty-intel <madhavi.losetty@intel.com>
2024-10-03 13:45:27 -0700

    fix(api): get, add and delete alarm works (#380)

--- internal/entity/dto/v1/alarm.go
+++ internal/entity/dto/v1/alarm.go
@@ -4,14 +4,15 @@ import "time"
 
 type (
 	AlarmClockOccurrence struct {
-		ElementName        string    `json:"elementName" binding:"required" example:"test"`
-		InstanceID         string    `json:"instanceID" binding:"" example:"test"`
-		StartTime          time.Time `json:"startTime" binding:"required" example:"2024-01-01T00:00:00Z"`
-		Interval           int       `json:"interval" binding:"number" example:"1"`
-		DeleteOnCompletion bool      `json:"deleteOnCompletion" binding:"required" example:"true"`
+		ElementName        string    `json:"ElementName" binding:"required" example:"test"`
+		InstanceID         string    `json:"InstanceID" binding:"" example:"test"`
+		StartTime          time.Time `json:"StartTime" binding:"required" example:"2024-01-01T00:00:00Z"`
+		Interval           int       `json:"Interval" default:"0" example:"1"`
+		IntervalInMinutes  int       `json:"IntervalInMinutes" example:"1"`
+		DeleteOnCompletion bool      `json:"DeleteOnCompletion" binding:"" example:"true"`
 	}
 	DeleteAlarmOccurrenceRequest struct {
-		InstanceID *string `json:"instanceID" binding:"" example:"test"`
+		Name string `json:"Name" binding:"required" example:"test"`
 	}
 
 	AddAlarmOutput struct {
