92b08a3876319c82df4d46c69fd260639c0a4403
Mike Johanson <michael.johanson@intel.com>
2024-11-05 14:10:45 -0700

    fix: redirect to login if 401 from server

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ version=$1
 docker build -t vprodemo.azurecr.io/console:v$version .
 
 # Build for Linux
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_linux_x64 ./cmd/app/main.go
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_linux_x64 ./cmd/app/main.go
 
 # Build for Windows
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_windows_x64.exe ./cmd/app/main.go
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_windows_x64.exe ./cmd/app/main.go
 
 # Build for Mac (x64)
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_mac_x64 ./cmd/app/main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_mac_x64 ./cmd/app/main.go
 
 # Build for Mac (arm64)
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_mac_arm64 ./cmd/app/main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w -X 'github.com/open-amt-cloud-toolkit/console/internal/app.Version=$version'" -trimpath -o console_mac_arm64 ./cmd/app/main.go
 
 # Mark the Unix system outputs as executable
 chmod +x console_linux_x64
